bbbb0cd23279c9400b313500082422b732c25308
Pascal Hurni <pascal_hurni@fastmail.fm>
2012-04-28 11:50:51 +0200

    Accomodate for connectedness links

--- TypeMarshaller.php
+++ TypeMarshaller.php
@@ -11,6 +11,8 @@
 class TypeMarshaller {
   
   protected $type_map = null;
+  
+  protected $link_object_name = 'link';
 
 	/**
 	 ** @param assoc $map a map of Resource type name to PHP model class name. Example: array('User' => 'User', 'Member' => 'User')
@@ -18,21 +20,32 @@ class TypeMarshaller {
 	 */
 	function __construct ($map, $options = array()) {
     $this->type_map = $map;
+    if (isset($options['link_object_name']) && $options['link_object_name']) {
+      $this->link_object_name = $options['link_object_name'];
+    }
 	}
 	
 	/**
 	 ** Creates an object of the PHP class found in the type map taken from the 'type' member of the passed $data assoc.
 	 ** 
 	 ** @param assoc $data the assoc (attributes => value) representation of the data to create. The value of any attribute may also be an assoc
-	 **                    in which case the objects are recursively created.
+	 **                    in which case the objects are recursively created except for an assoc having only a _link_ key.
 	 ** @return object The created instance
 	 */
 	function create_object($data) {
+	  // return null if we receive an empty object
+	  if (count((array)$data) == 0) {
+	    return null;
+	  }
+	  
 	  $class_name = $this->class_name_for($data->type);
     $obj = new $class_name;
     // FIXME: Currently no check is done if the keys are valid when populating the object
     foreach ($data as $key => $value) {
-      if (is_object($value)) {
+      if ($link = $this->_get_link($value)) {
+        $value = $link->name;
+      }
+      elseif (is_object($value)) {
         $value = $this->create_object($value);
       }
       $obj->set($key, $value);
@@ -58,6 +71,19 @@ class TypeMarshaller {
 	 */
 	function add_type($type, $class_name) {
 	  $this->map[$type] = $class_name;
+	}
+	
+	/*
+	 * Determine if the passed value is a Link and return it. Returns null if not a link.
+	 */
+	protected function _get_link($data) {
+    // Check if it is a Link
+    if (count((array)$data) == 1 && isset($data->{$this->link_object_name})) {
+      return $data->{$this->link_object_name};
+    }
+    else {
+      return null;
+    }
 	}
   
 }
